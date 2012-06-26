a1b27f59801a6803a679bdd4ba9166e1fb5f2677
edsammy <eddiemsamuels@gmail.com>
2012-06-26 17:24:20 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tact::Application.config.secret_token = 'bda7e7919e27776a13aeb7f8180f2cba460ef5f1689c4e3a6963cfefea6a1ee7d26fc563a12669bef7a4afe1f49010e51d2f12f5c3db0589ad80da6ad9f61400'
